c098940ae61014bb89f44e5f361465af43411ac8
daniel <daniel.inama02@gmail.com>
2025-04-13 11:53:54 +0200

    test

---
+++ stacks/qbittorrent/torrent.nix
@@ -0,0 +1,59 @@
+{ homelab, lib, ... }:
+
+{
+
+  sops.secrets."torrent.env" = {
+    sopsFile = ./qbittorrent.env;
+    format = "dotenv";
+    key = "";
+    restartUnits = [ "podman-qbittorrent.service" ];
+  };
+
+  myFolders = {
+
+    qbittorrent = {
+      path = "/home/ubuntu/qbittorrent";
+      owner = "ubuntu";
+      group = "users";
+      mode = "0755";
+    };
+  };
+  # Container
+  virtualisation.oci-containers.containers."qbittorrent" = {
+    image = "lscr.io/linuxserver/qbittorrent:latest";
+    log-driver = "journald";
+    environmentFiles = [
+      "/run/secrets/torrent.env"
+    ];
+    volumes = [
+      "/mnt/data/torrents:/downloads:rw"
+      "/home/ubuntu/qbittorrent:/config:rw"
+    ];
+    dependsOn = [
+      "seedbox-gluetun"
+    ];
+    extraOptions = [
+      "--network=container:gluetun"
+    ];
+  };
+
+  # Service
+  systemd.services."podman-qbittorrent" = {
+    serviceConfig = {
+      Restart = lib.mkOverride 90 "always";
+    };
+    after = [
+      "podman-network-seedbox_default.service"
+    ];
+    requires = [
+      "podman-network-seedbox_default.service"
+    ];
+    partOf = [
+      "podman-compose-seedbox-root.target"
+    ];
+    wantedBy = [
+      "podman-compose-seedbox-root.target"
+    ];
+  };
+
+}
