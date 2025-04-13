376a5d0e6584ba97a5e8741baed01fe6b061fbfe
daniel <daniel.inama02@gmail.com>
2025-04-13 15:15:17 +0200

    test

--- stacks/traefik/default.nix
+++ stacks/traefik/default.nix
@@ -52,8 +52,8 @@
     labels = {
       "traefik.enable" = "true";
       "traefik.http.routers.whoami.entrypoints" = "websecure";
-      "traefik.http.routers.whoami.rule" = "Host(`whoami.kuipr.de`)";
       "traefik.http.routers.whoami.middlewares" = "authelia@docker";
+      "traefik.http.routers.whoami.rule" = "Host(`whoami.kuipr.de`)";
       "traefik.http.routers.whoami.tls.certresolver" = "myresolver";
       "traefik.http.services.whoami.loadbalancer.server.port" = "80";
     };
