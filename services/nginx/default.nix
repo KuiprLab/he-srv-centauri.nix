8410307aee4fb74d8cdee134de0386e47d4e4d98
daniel <daniel.inama02@gmail.com>
2025-05-02 15:46:50 +0200

    test

--- services/nginx/default.nix
+++ services/nginx/default.nix
@@ -11,77 +11,72 @@
     recommendedOptimisation = true;
     recommendedProxySettings = true;
     recommendedTlsSettings = true;
-    
-    # Handle HTTP and HTTPS proxying similar to HAProxy setup
-    # This setting will ensure proper forwarding of client IP addresses
-    proxyResolveWhileRunning = false;
-    
-    # Stream configuration to handle TCP traffic like HAProxy does
-    streamConfig = ''
-      # This mimics HAProxy's TCP mode behavior
-      upstream hl_backend_ssl {
-        server 192.168.1.69:443;
-      }
-      
-      upstream k8s_backend_ssl {
-        server 192.168.1.200:443;
-      }
-      
-      upstream traefik_backend_ssl {
-        server 127.0.0.1:8443;
-      }
-      
-      # SSL/TLS routing based on SNI
-      map $ssl_preread_server_name $ssl_backend {
-        ~\.hl\.kuipr\.de$ hl_backend_ssl;
-        ~\.k8s\.kuipr\.de$ k8s_backend_ssl;
-        default traefik_backend_ssl;
-      }
-      
-      # HTTPS listener
-      server {
-        listen 443;
-        proxy_pass $ssl_backend;
-        ssl_preread on;
-      }
-    '';
-    
+
+    # Define Anubis upstream
+    upstreams."anubis" = {
+      servers = {
+        "unix:/run/anubis/nginx.sock" = {};
+      };
+    };
+
     virtualHosts = {
-      # HTTP virtual hosts
+      # HTTP redirects for all domains
       "hl.kuipr.de" = {
         serverName = "~^(.*\.)?hl\.kuipr\.de$";
-        listenAddresses = [ "0.0.0.0" ];
-        listen = [{ port = 80; addr = "0.0.0.0";}];
-        locations."/".proxyPass = "http://192.168.1.69:80";
-        locations."/".proxyWebsockets = true;
-        forceSSL = false; # We're handling SSL at the TCP level
-        enableACME = false; # Not needed with TCP SSL passthrough
+        locations."/".return = "301 https://$host$request_uri";
       };
-      
+
       "k8s.kuipr.de" = {
         serverName = "~^(.*\.)?k8s\.kuipr\.de$";
-        listenAddresses = [ "0.0.0.0" ];
-        listen = [{ port = 80; addr = "0.0.0.0"; }];
-        locations."/".proxyPass = "http://192.168.1.200:80";
-        locations."/".proxyWebsockets = true;
-        forceSSL = false; # We're handling SSL at the TCP level
-        enableACME = false; # Not needed with TCP SSL passthrough
+        locations."/".return = "301 https://$host$request_uri";
       };
-      
-      # Default HTTP backend for all other domains
+
+      # HTTPS virtual hosts with Anubis integration
+      "hl.kuipr.de-ssl" = {
+        serverName = "~^(.*\.)?hl\.kuipr\.de$";
+        forceSSL = true;
+        enableACME = true;
+        locations."/".proxyPass = "http://anubis";
+        locations."/".proxyWebsockets = true;
+      };
+
+      "k8s.kuipr.de-ssl" = {
+        serverName = "~^(.*\.)?k8s\.kuipr\.de$";
+        forceSSL = true;
+        enableACME = true;
+        locations."/".proxyPass = "http://anubis";
+        locations."/".proxyWebsockets = true;
+      };
+
+      # Default HTTPS backend
       "default" = {
         default = true;
-        listenAddresses = [ "0.0.0.0" ];
-        listen = [{ port = 80; addr = "0.0.0.0"; }];
-        locations."/".proxyPass = "http://127.0.0.1:8081";
+        forceSSL = true;
+        enableACME = true;
+        locations."/".proxyPass = "http://anubis";
         locations."/".proxyWebsockets = true;
       };
     };
   };
-  
-  
-  networking.firewall.allowedTCPPorts = [
-    80
-    443
+
+  # Anubis service configuration
+  services.anubis = {
+    enable = true;
+    settings = {
+      bind = "unix:/run/anubis/nginx.sock";
+      backends = [
+        { match = "host:hl.kuipr.de"; backend = "http://192.168.1.69:80"; }
+        { match = "host:k8s.kuipr.de"; backend = "http://192.168.1.200:80"; }
+        { default = true; backend = "http://127.0.0.1:8081"; }
+      ];
+      # Additional Anubis configuration as needed
+    };
+  };
+
+  networking.firewall.allowedTCPPorts = [ 80 443 ];
+
+  # Ensure Anubis socket directory exists
+  systemd.tmpfiles.rules = [
+    "d /run/anubis 0755 anubis anubis"
   ];
 }
