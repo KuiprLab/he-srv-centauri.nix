5f7370ac20960850ea74b228ad0d1e984e59ebb6
daniel <daniel.inama02@gmail.com>
2025-05-05 14:24:34 +0200

    add X-Real-IP header

--- services/nginx/default.nix
+++ services/nginx/default.nix
@@ -67,6 +67,12 @@
         # locations."/".proxyPass = "http://127.0.0.1:8081";
         locations."/".proxyPass = "http://unix:${config.services.anubis.instances.default.settings.BIND}";
         locations."/".proxyWebsockets = true;
+  extraConfig = ''
+    proxy_set_header Host $host;
+    proxy_set_header X-Real-IP $remote_addr;
+    proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+    proxy_set_header X-Forwarded-Proto $scheme;
+  '';
       };
     };
   };
