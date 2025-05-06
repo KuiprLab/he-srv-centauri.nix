efeccb206fbf6ed2724bfb9dd05f5d5e7d400cd8
daniel <daniel.inama02@gmail.com>
2025-05-06 10:56:24 +0200

    test

--- stacks/traefik/default.nix
+++ stacks/traefik/default.nix
@@ -56,6 +56,7 @@
       "traefik.http.routers.whoami.rule" = "Host(`whoami.kuipr.de`)";
       "traefik.http.routers.whoami.tls.certresolver" = "myresolver";
       "traefik.http.services.whoami.loadbalancer.server.port" = "80";
+    "traefik.http.routers.whoami.service"="whoami";
     };
     log-driver = "journald";
     extraOptions = [
