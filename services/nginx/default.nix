c9ca37d88dbfc6f26982232fa777c449ba1ef6be
daniel <daniel.inama02@gmail.com>
2025-05-06 10:00:40 +0200

    test

--- services/nginx/default.nix
+++ services/nginx/default.nix
@@ -26,7 +26,8 @@
 services.anubis = {
   instances = {
     default.settings = {
-      TARGET = "http://127.0.0.1:8081";
+      # TARGET = "http://127.0.0.1:8081";
+      TARGET = "https://127.0.0.1:8443";
       USE_REMOTE_ADDRESS = true;
       HOST_REWRITE = false;
       PRESERVE_HOST = true;
