11121853e7032ac6a17c74df738082368de63c66
daniel <daniel.inama02@gmail.com>
2025-07-25 17:30:41 +0200

    fix: try fix traefik warning for tls in path regexp

--- stacks/anubis/default.nix
+++ stacks/anubis/default.nix
@@ -35,12 +35,13 @@
     ];
     labels = {
       "traefik.docker.network" = "proxy"; # Telling Traefik which network to use
-      "traefik.http.routers.anubis.priority" = "1"; # Setting Anubis to the lowest priority, so it only takes the slack
       "traefik.http.routers.anubis.rule" = "PathRegexp(`.*`)"; # Wildcard match every path
+      "traefik.http.routers.anubis.priority" = "1"; # Setting Anubis to the lowest priority, so it only takes the slack
       "traefik.http.routers.anubis.entrypoints" = "websecure"; # Listen on HTTPS
-      "traefik.http.services.anubis.loadbalancer.server.port" = "8181"; # Telling Traefik to which port it should route requests
       "traefik.http.routers.anubis.service" = "anubis"; # Telling Traefik to use the above specified port
       "traefik.http.routers.anubis.tls.certresolver" = "myresolver"; # Telling Traefik to resolve a Cert for Anubis
+      "traefik.http.services.anubis.loadbalancer.server.port" = "8181"; # Telling Traefik to which port it should route requests
+      "traefik.http.routers.anubis.tls" = "true";
     };
     user = "0:0";
     log-driver = "journald";
