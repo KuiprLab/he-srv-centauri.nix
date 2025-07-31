557bddfe9777556aa9bda40a26fc03aed99f521a
daniel <daniel.inama02@gmail.com>
2025-07-31 11:31:40 +0200

    fix?

--- services/tailscale.nix
+++ services/tailscale.nix
@@ -3,6 +3,7 @@
   services.zabbixAgent = {
     enable = true;
     openFirewall = true;
+    server = "192.168.1.177";
     settings = {
       Hostname = "he-srv-centauri";
       ServerActive = "192.168.1.177";
