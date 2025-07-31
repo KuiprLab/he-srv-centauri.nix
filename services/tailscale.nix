4161aba2f0b3fa81ae51ac6acebe07c28c56283d
daniel <daniel.inama02@gmail.com>
2025-07-31 11:30:16 +0200

    enable zabbix

--- services/tailscale.nix
+++ services/tailscale.nix
@@ -1,3 +1,12 @@
 {
   services.tailscale.enable = true;
+  services.zabbixAgent = {
+    enable = true;
+    openFirewall = true;
+    settings = {
+      Hostname = "he-srv-centauri";
+      ServerActive = "192.168.1.177";
+      Servere = "127.0.0.1,192.168.1.177";
+    };
+  };
 }
