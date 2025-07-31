05999dda665a305cd0b4e2ef591764e048a7baf0
daniel <daniel.inama02@gmail.com>
2025-07-31 11:31:06 +0200

    fix: spelling mistake

--- services/tailscale.nix
+++ services/tailscale.nix
@@ -6,7 +6,7 @@
     settings = {
       Hostname = "he-srv-centauri";
       ServerActive = "192.168.1.177";
-      Servere = "127.0.0.1,192.168.1.177";
+      Server = "127.0.0.1,192.168.1.177";
     };
   };
 }
