6d70b979e260a236cc6ea4f1b5d0f9d777cc8899
daniel <daniel.inama02@gmail.com>
2025-08-01 12:42:52 +0200

    make work on docker?

--- modules/configuration.nix
+++ modules/configuration.nix
@@ -29,7 +29,10 @@
   };
 
   # Enable QEMU user emulation for x86_64
-  boot.binfmt.emulatedSystems = ["x86_64-linux"];
+  boot.binfmt = {
+    emulatedSystems = ["x86_64-linux" "i386-linux"];
+    preferStaticEmulators = true; # Make it work with Docker
+  };
 
   sops = {
     age.keyFile = "/var/lib/sops/age-key.txt";
