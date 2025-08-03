4e5acc7bb457c2630a461cdc566b2f8b9cdbf8b4
daniel <daniel.inama02@gmail.com>
2025-08-03 18:29:43 +0200

    changes

--- stacks/default.nix
+++ stacks/default.nix
@@ -14,6 +14,6 @@ _: {
     ./dozzle
     ./update-containers.nix
     ./explo
-    ./tmp
+    ./jellyseerr
   ];
 }
--- stacks/jellyseerr/default.nix
+++ stacks/jellyseerr/default.nix
@@ -16,7 +16,7 @@
 
   # Containers
   virtualisation.oci-containers.containers."jellyseerr" = {
-    image = "fallenbagel/jellyseerr:preview-OIDC";
+    image = "fallenbagel/jellyseerr:latest";
     environment = {
       "LOG_LEVEL" = "debug";
       "PORT" = "5055";
