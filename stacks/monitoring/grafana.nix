6a429bea2dad37af401ae10cb258b92801b0cbe5
daniel <daniel.inama02@gmail.com>
2025-09-12 09:23:34 +0200

    chore: update grafana

--- stacks/monitoring/grafana.nix
+++ stacks/monitoring/grafana.nix
@@ -19,7 +19,7 @@
 
   # Grafana configuration
   virtualisation.oci-containers.containers."grafana" = {
-    image = "docker.io/grafana/grafana-dev:12.2.0-257444";
+    image = "docker.io/grafana/grafana-dev:12.2.0-17660687647";
     environment = {
       "GF_SECURITY_ADMIN_USER" = "admin";
       "GF_USERS_ALLOW_SIGN_UP" = "false";
