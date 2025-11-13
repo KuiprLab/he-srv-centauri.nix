013cb0efd2b4a795c1a18966c7d1df70fa313123
daniel <daniel.inama02@gmail.com>
2025-11-13 10:34:42 +0100

    fix

--- stacks/default.nix
+++ stacks/default.nix
@@ -2,18 +2,12 @@ _: {
   imports = [
     ./anubis
     ./authelia
-    # ./explo
     ./gatus
     ./jellyfin
-    ./jellyseerr
     ./komga
     ./minecraft
     ./monitoring
-    # ./qbittorrent
-    # ./soulseek
-    # ./starr
     ./traefik
-    # ./cleanuparr
     ./zurg
   ];
 }
